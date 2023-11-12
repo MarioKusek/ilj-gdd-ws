576e7deeb2a4fc6a037d6c1a307cb3fb578a1702
MarioKusek <mario.kusek@fer.hr>
2023-11-12 14:08:53 +0100

    first commit

---
+++ src/main/java/hr/fer/ilj/ws/IljGddWsApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.ilj.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IljGddWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IljGddWsApplication.class, args);
+	}
+
+}
