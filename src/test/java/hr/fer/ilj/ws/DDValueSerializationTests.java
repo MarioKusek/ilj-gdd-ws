0fd74ccf99f81b04885adc50c995a6d1b8bb4b28
MarioKusek <mario.kusek@fer.hr>
2023-11-12 14:35:13 +0100

    add list of DDValue serialization test to JSON

--- src/test/java/hr/fer/ilj/ws/DDValueSerializationTests.java
+++ src/test/java/hr/fer/ilj/ws/DDValueSerializationTests.java
@@ -3,6 +3,7 @@ package hr.fer.ilj.ws;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +18,11 @@ class DDValueSerializationTests {
   @Autowired
   private JacksonTester<DDValue> json;
 
+  @Autowired
+  private JacksonTester<List<DDValue>> jsonList;
+
   @Test
-  void serializtion() throws Exception {
+  void serialization() throws Exception {
     DDValue value = new DDValue(LocalDate.of(2023, 10, 5), 1.56);
 
     String expectedContent = """
@@ -28,6 +32,33 @@ class DDValueSerializationTests {
             }
             """;
     assertThat(json.write(value)).isStrictlyEqualToJson(expectedContent);
+  }
+
+  @Test
+  void listSerialization() throws Exception {
+    List<DDValue> list = List.of(
+        new DDValue(LocalDate.of(2023, 10, 5), 1.56),
+        new DDValue(LocalDate.of(2023, 10, 6), 2.83),
+        new DDValue(LocalDate.of(2023, 10, 7), 3.21)
+        );
+
+    String expectedContent = """
+            [
+              {
+                "date":"2023-10-05",
+                "value":1.56
+              },
+              {
+                "date":"2023-10-06",
+                "value":2.83
+              },
+              {
+                "date":"2023-10-07",
+                "value":3.21
+              }
+            ]
+            """;
+    assertThat(jsonList.write(list)).isStrictlyEqualToJson(expectedContent);
   }
 
 }
